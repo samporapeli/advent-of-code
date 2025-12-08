13f0aa224d8ee8328fbcdf0ec126e5ad3cf27c6b
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2025-12-08 09:40:22 +0200

    08/2025

---
+++ 2025/08/solution-1.scala
@@ -0,0 +1,45 @@
+#!/usr/bin/env -S scala-cli -S 3
+
+// Usage: pbpaste | ./solution-1.scala
+
+import scala.io.StdIn
+import scala.collection.mutable.ArrayBuffer
+
+case class Jbox(x: Int, y: Int, z: Int)
+case class Distance(a: Int, b: Int, d: Double)
+case class JboxPair(a: Jbox, b: Jbox)
+
+object Solution:
+  def d(a: Jbox, b: Jbox) =
+    Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2) + Math.pow(a.z - b.z, 2))
+  def main(args: Array[String]): Unit =
+    // Parse puzzle input
+    val jboxes = Iterator.continually(StdIn.readLine())
+      .takeWhile(_ != null)
+      .filter(!_.isBlank)
+      .map(line =>
+        val coords = line.split(",").map(Integer.parseInt(_))
+        Jbox(coords(0), coords(1), coords(2))
+      )
+      .toVector
+    // Calculate distances between each box, order shortest first
+    val distances = (0 until jboxes.length).flatMap(y =>
+      (0 until jboxes.length).map(x =>
+        if (y >= x) null
+        else Distance(y, x, d(jboxes(y), jboxes(x)))
+      )
+    ).filter(_ != null)
+    .sortBy(_.d)
+
+    // Count circuits and connect jboxes
+    val circuits = jboxes.map(Set(_)).to(ArrayBuffer)
+    val boxesToAdd = distances
+      .take(1000)
+      .map(d => JboxPair(jboxes(d.a), jboxes(d.b)))
+    boxesToAdd.foreach(pair =>
+      val iA = circuits.indexWhere(set => set.contains(pair.a))
+      val iB = circuits.indexWhere(set => set.contains(pair.b))
+      circuits(iA) = circuits(iA) ++ circuits(iB)
+      if (iA != iB) circuits.remove(iB)
+    )
+    println(circuits.map(_.size).sorted.reverse.take(3).product)
